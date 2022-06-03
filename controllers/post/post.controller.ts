0d8c1403e84c577dc6d60b15491afc3521b596e0
Sojeb Sikder <sojebsikder@gmail.com>
2022-06-03 17:05:22 +0600

    main

--- controllers/post/post.controller.ts
+++ controllers/post/post.controller.ts
@@ -4,41 +4,46 @@ import { PostService } from "./post.service";
 
 export class PostController {
   // constructor(private readonly postService: PostService) {}
-  constructor() {}
+  constructor(private postService: PostService) {}
   /**
    * show all data
    * @param req
    * @param res
    */
-  async index(req: Request, res: Response) {
-    const result = await new PostService().index();
+  // async index(req: Request, res: Response) {
+  //   // const result = await new PostService().index();
+  //   const result = await this.postService.index();
+  //   res.render("index", { posts: result });
+  // }
+  index = async (req: Request, res: Response) => {
+    const result = await this.postService.index();
     res.render("index", { posts: result });
-  }
+  };
 
   /**
    * show specific data
    * @param req
    * @param res
    */
-  async show(req: Request, res: Response) {
+  show = async (req: Request, res: Response) => {
     const id = req.params.id;
-    const result = await new PostService().show(id);
+    const result = await this.postService.show(id);
     res.locals.title = `${result.title} - ${env("APP_NAME")}`;
     res.render("post/postSingle", { post: result });
-  }
+  };
 
   /**
    * store data
    * @param req
    * @param res
    */
-  async store(req: Request, res: Response) {
-    await new PostService().store(req, res);
+  store = async (req: Request, res: Response) => {
+    await this.postService.store(req, res);
 
     res.render("post/addPost", {
       message: "Post has been added successfully",
     });
-  }
+  };
 
   /**
    * show add post page
--- routes/postRouter.ts
+++ routes/postRouter.ts
@@ -1,10 +1,11 @@
 import express from "express";
 import { PostController } from "../controllers/post/post.controller";
+import { PostService } from "../controllers/post/post.service";
 
 import { decorateHtmlResponse } from "../middlewares/common/decorateHtmlResponse";
 
 const router = express.Router();
-const controller = new PostController();
+const controller = new PostController(new PostService);
 
 router.get("/", decorateHtmlResponse(), controller.index);
 router.get("/post/add", decorateHtmlResponse(), controller.showAddPostPage);
