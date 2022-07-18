9354a505000d3654befceb4f4902483d93234ced
sojebsikder <sojebsikder@gmail.com>
2022-07-18 12:50:59 +0600

    main

--- app/middlewares/common/upload.ts
+++ app/middlewares/common/upload.ts
@@ -1,22 +1,45 @@
 import { Uploader } from "../../../system/src/core/Disk";
 
-export function attachmentUpload(req, res, next) {
-  const upload = Uploader.upload(
-    "images",
-    ["image/jpeg", "image/jpg", "image/png"],
-    1000000,
-    2,
-    "Only .jpg, jpeg or .png format allowed!"
-  );
+type fields = {
+  /**
+   * field name
+   */
+  name: string;
+  /**
+   * max upload file number
+   */
+  maxCount: number;
+};
 
-  // call the middleware function
-  upload.any()(req, res, (err) => {
-    if (err) {
-      res.status(500).json({
-        message: err.message,
-      });
-    } else {
-      next();
-    }
-  });
+type attachmentOption = {
+  /**
+   * field name
+   */
+  fieldname: fields[];
+  /**
+   * distination path
+   */
+  distination: string;
+};
+export function attachmentUpload(attachmentOption: attachmentOption) {
+  return function (req, res, next) {
+    const upload = Uploader.upload(
+      attachmentOption.distination,
+      ["image/jpeg", "image/jpg", "image/png"],
+      1000000,
+      2,
+      "Only .jpg, jpeg or .png format allowed!"
+    );
+
+    // call the middleware function
+    upload.fields(attachmentOption.fieldname)(req, res, (err) => {
+      if (err) {
+        res.status(500).json({
+          message: err.message,
+        });
+      } else {
+        next();
+      }
+    });
+  };
 }
