dff03ab66239741c1be5149a8fd98b37f6091cf5
sojebsikder <sojebsikder@gmail.com>
2023-04-01 20:39:02 +0600

    updated cli.js

--- bin/cli.js
+++ bin/cli.js
@@ -1,6 +1,29 @@
 #!/usr/bin/env node
-
+const fs = require("fs");
 const { execSync } = require("child_process");
+// const readline = require("readline");
+
+// const rl = readline.createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+
+const deleteDir = (path) => {
+  try {
+    fs.rmSync(path, { recursive: true, force: true });
+  } catch (error) {}
+};
+const deleteFile = (path) => {
+  try {
+    fs.unlinkSync(path);
+  } catch (error) {}
+};
+
+const copyFile = (src, dest) => {
+  try {
+    fs.copyFileSync(src, dest);
+  } catch (error) {}
+};
 
 const runCommand = (command) => {
   try {
@@ -12,39 +35,67 @@ const runCommand = (command) => {
   return true;
 };
 
+let pmName = "pnpm";
 const repoName = process.argv[2];
 if (!repoName) {
   console.error("Please provide a project name");
   process.exit(-1);
 }
 const gitCheckoutCommand = `git clone --depth 1 https://github.com/SojebSikder/create-sojeb-express-ts-app ${repoName}`;
-const installDepsCommand = `cd ${repoName} && yarn install`;
+const installDepsCommand = `cd ${repoName} && ${pmName} install`;
+
+// ask which pm to use
+// console.log("Select package manager");
+// console.log(`1 -> pnpm`);
+// console.log(`2 -> yarn`);
+// console.log(`3 -> npm`);
+
+// rl.question("Select package manager ", function (value) {
+//   switch (value) {
+//     case "1":
+//       console.log(`1 -> pnpm: selected`);
+//       pmName = "pnpm";
+//       break;
+//     case "2":
+//       console.log(`2 -> yarn: selected`);
+//       pmName = "yarn";
+//       break;
+//     case "3":
+//       console.log(`3 -> npm: selected`);
+//       pmName = "npm";
+//       break;
+//     default:
+//       console.log(`3 -> npm: selected`);
+//       pmName = "npm";
+//       break;
+//   }
+//   rl.close();
+// });
+// rl.on("close", function () {
+//   process.exit(0);
+// });
 
 console.log(`Creating new project ${repoName}`);
 const checkedOut = runCommand(gitCheckoutCommand);
 if (!checkedOut) process.exit(-1);
 
 // console.log(`deleting lock files for ${repoName}`);
-const deletedLockFile = runCommand(`rm -rf ${repoName}/yarn.lock`);
-if (!deletedLockFile) process.exit(-1);
+deleteFile(`${repoName}/yarn.lock`);
+deleteFile(`${repoName}/package-lock.json`);
+deleteFile(`${repoName}/pnpm-lock.yaml`);
 
-const deletedNpmLockFile = runCommand(`rm -rf ${repoName}/package-lock.json`);
-if (!deletedNpmLockFile) process.exit(-1);
-
-console.log(`Installing dependencies for ${repoName}`);
-const installedDeps = runCommand(installDepsCommand);
-if (!installedDeps) process.exit(-1);
+// console.log(`Installing dependencies for ${repoName}`);
+// const installedDeps = runCommand(installDepsCommand);
+// if (!installedDeps) process.exit(-1);
 
 console.log(`copying .env.example to .env`);
-const copiedEnv = runCommand(`cp ${repoName}/.env.example ${repoName}/.env`);
-// if (!copiedEnv) process.exit(-1);
-
-// console.log(`Deleting bin folder`);
-const deletedBin = runCommand(`rm -rf ${repoName}/bin`);
-// if (!deletedBin) process.exit(-1);
-const deletedGithub = runCommand(`rm -rf ${repoName}/.github`);
+copyFile(`${repoName}/.env.example`, `${repoName}/.env`);
+console.log(`deleting unnecessary files for ${repoName}`);
+deleteDir(`${repoName}/bin`);
+deleteDir(`${repoName}/.github`);
+deleteDir(`${repoName}/.git`);
 
 console.log(
   `${repoName} created successfully. Follow the following commands to start`
 );
-console.log(`cd ${repoName} && yarn start:dev`);
+console.log(`cd ${repoName} && ${pmName} start:dev`);
